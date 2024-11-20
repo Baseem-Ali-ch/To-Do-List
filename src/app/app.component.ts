3756c1f5039d7f9b962c8ed1d8e28ec7fddcbdd5
Baseem ali ch <baseemch43@gmail.com>
2024-11-20 10:43:49 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,79 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor, NgClass, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'To-do-list';
+  todoList: TodoItem[] = [];
+
+  newTask: string = '';
+
+  // Tracking id
+  editingTaskId: number | null = null;
+  editingTaskValue: string = '';
+
+  // Add task function
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false,
+      };
+
+      this.todoList.push(newTodoItem);
+      this.newTask = '';
+    }
+  }
+
+  // Complete task
+  toggleCompleted(index: number): void {
+    console.log(index);
+
+    this.todoList[index].completed = !this.todoList[index].completed;
+    console.log(this.todoList);
+  }
+
+  // Delete task
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter((item) => item.id !== id);
+    console.log(this.todoList);
+  }
+
+  // Edit task
+  editTask(id: number): void {
+    const taskToEdit = this.todoList.find((item) => item.id === id);
+    if (taskToEdit) {
+      this.editingTaskId = id;
+      this.editingTaskValue = taskToEdit.task;
+    }
+  }
+
+  // Save edited task
+  saveTask(id: number): void {
+    const taskToEdit = this.todoList.find((item) => item.id === id);
+    if (taskToEdit) {
+      taskToEdit.task = this.editingTaskValue;
+      this.editingTaskId = null;
+      this.editingTaskValue = ''
+    }
+  }
+
+  // Cancel editing
+  cancelEdit(): void{
+    this.editingTaskId = null
+    this.editingTaskValue = ''
+  }
 }
